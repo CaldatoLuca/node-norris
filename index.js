7eb448641a25b9d71d4d8ade637b54d5ee4b48cd
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-23 14:55:06 +0200

    feat: server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("ciao");
+require("dotenv").config();
+
+const http = require("http");
+
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html", "utf-8");
+  res.end("Hello World");
+});
+
+server.listen(port, host, () => {
+  console.log(`Server partito al link http://${host}:${port}/`);
+});
